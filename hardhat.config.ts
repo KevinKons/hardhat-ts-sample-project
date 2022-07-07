c63da68f43ad260dfffb5cd97897b0f4a2cc9830
kevin kons <kevin.kons@hotmail.com>
2022-07-07 15:13:37 -0300

    Adding networks and etherscan to config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,30 @@
+import * as dotenv from 'dotenv';
+
 import { HardhatUserConfig } from 'hardhat/config';
 import '@typechain/hardhat'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
+import '@nomiclabs/hardhat-etherscan';
 
+dotenv.config();
+
+const ACCOUNT_ONE: any = process.env.ACCOUNT_ONE;
+const ROPSTEN_URL: any = process.env.ROPSTEN_URL;
+const ROPSTEN_ETHERSCAN_API_KEY: any = process.env.ROPSTEN_ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.10",
+  networks: {
+    ropsten: {
+      url: ROPSTEN_URL,
+      accounts: [ACCOUNT_ONE]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      ropsten: ROPSTEN_ETHERSCAN_API_KEY,
+    }
+  },
 };
 
 export default config;
